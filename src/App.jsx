4d7e93ba0c0706375aea153abc93f143c60761bc
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-10-25 00:03:10 +0200

    feat: secure storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   function sendPushNotificationFromWebPage() {
-    window.ReactNativeWebView.postMessage('sendPushNotification');
+    window.ReactNativeWebView.postMessage(JSON.stringify({ action: 'sendPushNotification'}));
+  }
+
+  function saveDataToSecureStorage() {
+    window.ReactNativeWebView.postMessage(JSON.stringify({ action: 'saveData', data: count }));
+  }
+
+  function retrieveDataFromSecureStorage() {
+    window.ReactNativeWebView.postMessage(JSON.stringify({ action: 'retrieveData' }));
   }
 
   return (
@@ -19,7 +30,22 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={sendPushNotificationFromWebPage}>Send Push Notification</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Current count is {count}
+        </button>
+        {typeof window !== 'undefined' && (
+          <>
+            <button onClick={saveDataToSecureStorage}>
+              Save count
+            </button>
+            <button onClick={retrieveDataFromSecureStorage}>
+              Get count
+            </button>
+            <button onClick={sendPushNotificationFromWebPage}>
+              Send Push Notification
+            </button>
+          </>
+        )}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,7 +54,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
