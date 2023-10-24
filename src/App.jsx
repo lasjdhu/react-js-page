611d727a2dd7c7d6837666cac9bfb50f9f9bad90
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-10-24 23:35:44 +0200

    feat: notification button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function sendPushNotificationFromWebPage() {
+    window.ReactNativeWebView.postMessage('sendPushNotification');
+  }
 
   return (
     <>
@@ -18,9 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={sendPushNotificationFromWebPage}>Send Push Notification</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
