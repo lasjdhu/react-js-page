ba9de9752c33b35ca932cebba92d9180d3c9f162
Dmitrii Ivanushkin <dmitrii.ivanushkin1@gmail.com>
2023-10-25 23:08:02 +0200

    styles: vite -> expo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import expoLogo from '/expoio-icon.svg';
 import './App.css';
 
 function App() {
@@ -21,14 +22,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://expo.dev" target="_blank" rel="noreferrer">
+          <img src={expoLogo} className="logo" alt="Expo logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Expo WebView + React.js</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Current count is {count}
@@ -46,13 +47,7 @@ function App() {
             </button>
           </>
         )}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
